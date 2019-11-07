3e5522bc0b83c2fd7cc4c0d0c7761f126b68518e
MJ Kochuk <mjkochuk@gmail.com>
2019-11-07 13:45:39 -0700

    Secondary filter for user servlet implemented

--- src/java/filters/AdminFilter.java
+++ src/java/filters/AdminFilter.java
@@ -9,6 +9,8 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -16,6 +18,11 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import models.User;
+import services.UserService;
 
 /**
  *
@@ -26,6 +33,36 @@ public class AdminFilter implements Filter {
             FilterChain chain)
             throws IOException, ServletException {
         
+        // code that is executed before the servlet
+        
+        HttpServletRequest hsr = (HttpServletRequest)request;
+        HttpSession session = hsr.getSession();
+        
+        UserService us = new UserService();
+        HttpServletResponse hsre = (HttpServletResponse)response;
+        
+        if (session.getAttribute("email") == null) {
+            hsre.sendRedirect("login");
+            return;
+        } 
+        
+        try {
+            User currUser = us.get((String) session.getAttribute("email"));
+            if (!(currUser.getRole().getRoleID() == 1 || currUser.getRole().getRoleID() == 3)){
+            hsre.sendRedirect("home");
+            return;
+        }
+        } catch (Exception ex) {
+            Logger.getLogger(AdminFilter.class.getName()).log(Level.INFO, (String) session.getAttribute("email"), ex);
+            hsre.sendRedirect("login");
+            return;
+        }
+        
+        
+         // allow the user to access the servlet
+         chain.doFilter(request, response);
+         
+         // code that is executed after the servlet
        
     }
     public void destroy() {
